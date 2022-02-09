5be1cf5695736482244abd61553afae13cb5227e
Osvaldinei Oliveira <valdinei.olv@gmail.com>
2022-02-09 17:07:09 -0300

    Implementado IconBadge

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/widgets/icon_badge.dart';
 import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_appbar.dart';
 import './home_controller.dart';
 
@@ -17,8 +18,12 @@ class HomePage extends GetView<HomeController> {
           items: [
             const BottomNavigationBarItem(
                 icon: Icon(Icons.list), label: 'Produtos'),
-            const BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart), label: 'Carrinho'),
+            BottomNavigationBarItem(
+                icon: IconBadge(
+                  icon: Icons.shopping_cart,
+                  number: controller.totalProductsInShoppingCard,
+                ),
+                label: 'Carrinho'),
             const BottomNavigationBarItem(
               icon: Icon(Icons.exit_to_app),
               label: 'Sair',
